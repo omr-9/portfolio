520556880c8006fe39a0d76a3648fded5e110ae3
omr-9 <omaralsharkawi9@gmail.com>
2025-05-01 21:37:13 +0300

    fix import and edit meta of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/home"
+
 import NotFound from "./pages/NotFound"
 import { Toaster } from "@/components/ui/toaster";
+import Home from "./pages/Home";
 
 function App() {
 
